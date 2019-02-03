7065d1a62ef7f9ae15ec8d3730253ba49c84134c
Mark Michaelis <mark.michaelis@coremedia.com>
2019-02-03 21:46:58 +0100

    First PoC-Commit
    Contains some examples derived from example provided by Baeldung.

---
+++ grpc-parent/grpc-server/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module grpc.server {
+  requires com.google.common;
+  requires java.annotation;
+  requires java.compiler;
+  requires grpc.core;
+  requires grpc.protobuf;
+  requires grpc.stub;
+  requires protobuf.java;
+}
