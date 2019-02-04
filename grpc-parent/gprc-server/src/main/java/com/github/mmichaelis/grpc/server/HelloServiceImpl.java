942a04b2e9752f71b7925ec058e5ea908ca2d285
Mark Michaelis <mark.michaelis@coremedia.com>
2019-02-04 22:14:37 +0100

    Small extension to Server Mr./Ms.

--- grpc-parent/gprc-server/src/main/java/com/github/mmichaelis/grpc/server/HelloServiceImpl.java
+++ grpc-parent/gprc-server/src/main/java/com/github/mmichaelis/grpc/server/HelloServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.mmichaelis.grpc.server;
 import com.github.mmichaelis.grpc.service.HelloRequest;
 import com.github.mmichaelis.grpc.service.HelloResponse;
 import com.github.mmichaelis.grpc.service.HelloServiceGrpc;
+import com.github.mmichaelis.grpc.service.Person;
 import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import io.grpc.stub.StreamObserver;
@@ -11,15 +12,31 @@ import io.grpc.stub.StreamObserver;
 public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
   @Override
   public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
-    String greeting = new StringBuilder()
-            .append("Hello, ")
-            .append(request.getPerson().getFirstName())
+    StringBuilder builder = new StringBuilder();
+    Person person = request.getPerson();
+
+    builder.append("Hello, ");
+    switch (person.getGender()) {
+      case MALE:
+        builder.append("Mr. ");
+        break;
+      case FEMALE:
+        builder.append("Ms. ");
+        break;
+      case OTHER:
+        break;
+      case UNRECOGNIZED:
+        builder.append("Mr. or Ms. ");
+      default:
+        // nothing to do
+    }
+    builder.append(person.getFirstName())
             .append(" ")
-            .append(request.getPerson().getLastName())
+            .append(person.getLastName())
             .toString();
 
     HelloResponse response = HelloResponse.newBuilder()
-            .setGreeting(greeting)
+            .setGreeting(builder.toString())
             .build();
 
     responseObserver.onNext(response);
